5e8e436028ff764f5d7b887dd07f7e2353d17907
Kazuma Fujita <kazuma.fujita@genova.co.jp>
2019-01-08 12:12:34 +0900

    Initial Commit

---
+++ LoginPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoginPractice
+//
+//  Created by 藤田和磨 on 2019/01/08.
+//  Copyright © 2019 藤田和磨. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ LoginPracticeTests/LoginPracticeTests.swift
@@ -0,0 +1,34 @@
+//
+//  LoginPracticeTests.swift
+//  LoginPracticeTests
+//
+//  Created by 藤田和磨 on 2019/01/08.
+//  Copyright © 2019 藤田和磨. All rights reserved.
+//
+
+import XCTest
+@testable import LoginPractice
+
+class LoginPracticeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
