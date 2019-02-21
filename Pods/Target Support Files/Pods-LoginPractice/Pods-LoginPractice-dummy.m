4145f992cb3a2546ed032ce624d375c3cd49bd42
kazuma-fujita-genova <kazuma.fujita@genova.co.jp>
2019-02-21 19:26:10 +0900

    めんどくさいのでPodごとPush

---
+++ Pods/Target Support Files/Pods-LoginPractice/Pods-LoginPractice-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LoginPractice : NSObject
+@end
+@implementation PodsDummy_Pods_LoginPractice
+@end
---
+++ Pods/Target Support Files/Pods-LoginPracticeTests/Pods-LoginPracticeTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LoginPracticeTests : NSObject
+@end
+@implementation PodsDummy_Pods_LoginPracticeTests
+@end
